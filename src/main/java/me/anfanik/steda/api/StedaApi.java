1e313bd7b330d68ee2d6243a9f82564f5f115269
Andrew Anfanik <me@anfanik.me>
2021-05-13 00:12:56 +0300

    Add sharkly integration

--- src/main/java/me/anfanik/steda/api/StedaApi.java
+++ src/main/java/me/anfanik/steda/api/StedaApi.java
@@ -1,9 +1,11 @@
 package me.anfanik.steda.api;
 
+import me.anfanik.sharkly.utility.Formatter;
 import me.anfanik.steda.api.chat.ChatInputService;
 import me.anfanik.steda.api.command.exact.PingCommand;
 import me.anfanik.steda.api.menu.MenuService;
 import me.anfanik.steda.api.sidebar.SidebarService;
+import me.anfanik.steda.api.utility.TextUtility;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
@@ -20,6 +22,11 @@ public class StedaApi {
         new MenuService(plugin, pluginManager);
         new ChatInputService(plugin, pluginManager);
         new PingCommand();
+
+        if (pluginManager.getPlugin("Sharkly") != null) {
+            plugin.getLogger().info("Initializing a integration with Sharkly.");
+            Formatter.addFormatter(TextUtility::colorize);
+        }
     }
 
 }
